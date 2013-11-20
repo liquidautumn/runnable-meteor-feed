0c964ffad11635028e6a3ee75c8be0a2b1f95385
Denis Grankin <liquidautumn@gmail.com>
2013-11-20 20:08:10 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,17 @@
+Meteor feed example
+===================
+
+Meteor.js external REST API access.
+
+Setup on Runnable.com
+-------------------
+Create Node.js Runnable and install Meteor using quickstart
+```
+curl https://gist.github.com/liquidautumn/7398127/raw/runnable-meteor | /bin/sh
+```
+Clone example repository to `app` folder
+```
+git clone https://github.com/liquidautumn/runnable-meteor-feed.git app
+```
+Open dropdown on the Run button and set Run cmd to `./start`
+Run it.
---
+++ app.js
@@ -0,0 +1,35 @@
+Commits = new Meteor.Collection('commits');
+
+if (Meteor.isClient) {
+    Template.commitlist.commits = function () {
+        return Commits.find({}, { sort: { createdAt: 1 }});
+    };
+
+    Handlebars.registerHelper("prettyDate", function(timestamp) {
+        return (new Date(timestamp)).toString('yyyy-MM-dd hh:mm:ss');
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        Meteor.setInterval(function () {
+            Meteor.http.get("https://api.github.com/repos/meteor/meteor/commits", function (error, result) {
+                if (result.statusCode === 200) {
+                    for (var i = 0; i < result.data.length; i++) {
+                        var c = result.data[i];
+                        if (Commits.find({sha: c.sha}).fetch().length == 0){
+                            Commits.insert({
+                                sha: c.sha,
+                                author: c.commit.author.name,
+                                message: c.commit.message,
+                                createdAt: Date.now()
+                            });
+                        }
+                    }
+                } else {
+                    console.log(result);
+                }
+            });
+        }, 1000 * 60 * 5);
+    });
+}
